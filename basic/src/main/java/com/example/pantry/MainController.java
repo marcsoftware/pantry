092631175473d00127678297ba04e2686491a752
marcsoftware <you@example.com>
2020-02-06 16:34:19 -0700

    can recieve json

--- basic/src/main/java/com/example/pantry/MainController.java
+++ basic/src/main/java/com/example/pantry/MainController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -42,4 +44,11 @@ public class MainController {
 
 		return FoodRepository.findAll();
 	}
+
+
+	@PostMapping(path= "/json",consumes = "application/json")
+	public @ResponseBody String update(@RequestBody Food food) {
+		return "yes";
+	}
 }
+
