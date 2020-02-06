94a6ec50011f264f1a9a2460dfc677260b429011
marcsoftware <you@example.com>
2020-02-06 10:27:24 -0700

    saves to mongodb

---
+++ basic/src/main/js/app.js
@@ -0,0 +1,136 @@
+
+
+// tag::vars[]
+const React = require('react');
+const ReactDOM = require('react-dom');
+const client = require('./client');
+// end::vars[]
+//import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+  } from "react-router-dom";
+
+
+
+// tag::app[]
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {employees: []};
+	}
+
+	componentDidMount() {
+		client({method: 'GET', path: '/demo/all'}).done(response => {
+			this.setState({employees: response.entity});
+		});
+	}
+
+	render() {
+		return (
+			<>
+			<Home/>
+			<EmployeeList employees={this.state.employees}/>
+			</>
+		)
+	}
+}
+// end::app[]
+
+// tag::employee-list[]
+class EmployeeList extends React.Component{
+	//<Employee key={employee._links.self.href} employee={employee}/>
+	render() {
+		const employees = this.props.employees.map(employee =>
+			<Employee key={employee.id} employee={employee}/>
+			
+		);
+		return (
+			<table>
+				app
+				<tbody>
+					
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Description</th>
+					</tr>
+					{employees}
+				</tbody>
+			</table>
+		)
+	}
+}
+// end::employee-list[]
+
+// tag::employee[]
+class Employee extends React.Component{
+	render() {
+		return (
+			<>
+			
+			<tr>
+				<td>{this.props.employee.name}</td>
+				<td>{this.props.employee.id}</td>
+				<td>{this.props.employee.email}</td>
+			</tr>
+			</>
+		)
+	}
+}
+// end::employee[]
+
+// tag::render[]
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+)
+// end::render[]
+
+function Blank(){
+	return <h3>blank</h3>
+}
+
+function Blank2(){
+	return <h3>blank2</h3>
+}
+
+function Blank3(){
+	return <h3>blank3</h3>
+}
+
+function Home() {
+	return <Router>
+	<div>
+	  app
+	  <nav>
+		<ul>
+		  <li>
+			<Link to="/">home</Link>
+		  </li>
+		  <li>
+			<Link to="/two">two</Link>
+		  </li>
+		  <li>
+			<Link to="/three">three</Link>
+		  </li>
+		</ul>
+	  </nav>
+
+	  {/* A <Switch> looks through its children <Route>s and
+		  renders the first one that matches the current URL. */}
+	  <Switch>
+	   
+		<Route path="/two">
+		  <Blank2 />
+		</Route>
+		<Route path="/three">
+		  <Blank3 />
+		</Route>
+	  </Switch>
+	</div>
+  </Router>
+  }
---
+++ basic/src/main/js/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const base = <h1>Hello, world<App/></h1>;
+
+ReactDOM.render(<base />, document.getElementById('root'));
+
+
+
+
