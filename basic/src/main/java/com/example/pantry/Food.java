de44aee39d4d49bd3a1af9a25333485cd85bfcb1
marcsoftware <you@example.com>
2020-02-06 14:37:56 -0700

    edited fields on FOOD entity

--- basic/src/main/java/com/example/pantry/Food.java
+++ basic/src/main/java/com/example/pantry/Food.java
@@ -9,20 +9,44 @@ public class Food{
 	public String id;
 
 	public String name;
-	public String email;
+	public String consumed_calories;
+	public String consumed_label;
+	public String consumed_unit;
+
+	public String ratio_calories;
+	public String ratio_label;
+	public String ratio_unit;
 
 	public Food() {}
 
-	public Food(String name, String email) {
+	public Food(String name, 
+				String consumed_calories,
+				String consumed_label,
+				String consumed_unit,
+
+				String ratio_calories,
+				String ratio_label,
+				String ratio_unit
+				 
+	) {
 		this.name = name;
-		this.email = email;
+
+		this.consumed_calories=consumed_calories;
+		this.consumed_label=consumed_label;		
+		this.consumed_unit=consumed_unit;
+
+		this.ratio_calories=ratio_calories;
+		this.ratio_label=ratio_label;
+		this.ratio_unit=ratio_unit;
+
+	
 	}
 
 	@Override
 	public String toString() {
 		return String.format(
-				"Customer[id=%s, name='%s', email='%s']",
-				id, name, email);
+				"Customer[id=%s, name='%s', calories='%s']",
+				id, name, consumed_calories);
 	}
 
 }
--- basic/src/main/java/com/example/pantry/MainController.java
+++ basic/src/main/java/com/example/pantry/MainController.java
@@ -25,7 +25,7 @@ public class MainController {
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
 
-	  Food c = new Food(name,email);
+	  Food c = new Food(name,email,"X","X","X","X","X");
 	
 	
 	  FoodRepository.save(c);
