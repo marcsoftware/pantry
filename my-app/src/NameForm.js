f98fcdf868a724d46f830377da198b6dc3722379
marcsoftware <marcdavidmelcher@gmail.com>
2020-03-04 16:04:47 -0700

    automatically add label as ratio

--- my-app/src/NameForm.js
+++ my-app/src/NameForm.js
@@ -216,6 +216,12 @@ class NameForm extends React.Component {
 		if(food.ratio_label===food.consumed_label && Number.isNaN(food.consumed_calories)){
 			food.consumed_calories=food.ratio_calories*(food.consumed_unit/food.ratio_unit);
 		}
+alert(food.ratio_label);
+		if( food.ratio_label == undefined){
+			food.ratio_label=food.consumed_label;
+			food.ratio_unit=food.consumed_unit;
+			food.ratio_calories=food.consumed_calories;
+		}
 		
 		return food;
 	}
