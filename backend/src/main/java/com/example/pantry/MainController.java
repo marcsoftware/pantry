8e2faa75f44e8c7fa8430bfd12a7141e4495f7dd
marcsoftware <marcdavidmelcher@gmail.com>
2020-11-19 12:55:22 -0600

    auth endpoint works & tested

--- backend/src/main/java/com/example/pantry/MainController.java
+++ backend/src/main/java/com/example/pantry/MainController.java
@@ -124,9 +124,42 @@ public class MainController {
 
 
 
+		// idTokenString has to be a valid-format token. A random string will cause illegal argument exception.
+		GoogleIdToken idToken;
+		try {
+			 idToken = verifier.verify(idTokenString);
+		}catch(Exception  e){
+			return "idToken string was not in the valid format. length should be 101 and no invalid symbols";
+		}
 
-		GoogleIdToken idToken = verifier.verify(idTokenString);
-		return "here 120";
+
+		if (idToken != null) {
+	/*
+			Payload payload = idToken.getPayload();
+
+			// Print user identifier
+			String userId = payload.getSubject();
+			System.out.println("User ID: " + userId);
+
+			// Get profile information from payload
+			String email = payload.getEmail();
+			boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+			String name = (String) payload.get("name");
+			String pictureUrl = (String) payload.get("picture");
+			String locale = (String) payload.get("locale");
+			String familyName = (String) payload.get("family_name");
+			String givenName = (String) payload.get("given_name");
+
+			// Use or store profile information
+			// ...
+	*/
+
+			return("VALID ID token.");
+		} else {
+				return("Invalid ID token.");
+		}
+
+
 
 	}
 
