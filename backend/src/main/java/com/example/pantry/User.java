f8bef96ebd58a18e9416f70f077187b3ae7670fa
marcsoftware <marcdavidmelcher@gmail.com>
2021-01-13 14:11:14 -0600

    hibernate made column email unqiue

--- backend/src/main/java/com/example/pantry/User.java
+++ backend/src/main/java/com/example/pantry/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Column;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
@@ -13,6 +14,7 @@ public class User {
 
 	public String name;
 
+	@Column(unique = true)
 	public String userEmail;
 
  	public String idTokenString;
